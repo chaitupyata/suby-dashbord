c8df681fc8a5c50177fb139d0c68fb147a809594
chaitupyata <chaitupyata@gmail.com>
2025-03-31 19:38:39 +0530

    commit

--- src/vendorDashboard/components/utilities/ApiPath.js
+++ src/vendorDashboard/components/utilities/ApiPath.js
@@ -1,5 +1,5 @@
 
-export const API_URL = "http://localhost:4000";
+// export const API_URL = "http://localhost:4000";
 
 
-// export const API_URL = "https://suby-backend-5yc8.onrender.com"
+export const API_URL = "https://suby-backend-5yc8.onrender.com"
